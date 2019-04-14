36192c4bb3ae06c6b742417db25745ec40d6c902
c1294103 <lcsantos1@stefanini.com>
2019-04-14 20:45:26 -0300

    aprendendo sobre travis integração continua

---
+++ spec/exemplo/exemploSpec.js
@@ -0,0 +1,9 @@
+describe("Suite de teste de exemplo com NodeJS", function () {
+    it("Deve demonstrar o uso do Jasmine com o NodeJS", function () {
+        expect(true).toEqual(true);
+    })
+
+    it("Deve", function () {
+        expect(1).toEqual(1);
+    })
+});
